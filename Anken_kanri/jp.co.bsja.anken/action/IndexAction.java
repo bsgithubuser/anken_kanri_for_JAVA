bf86a59a114004e6e0d349111bbc1e32c386e774
kensyu_2 <kensyu_2@BS021>
2016-09-07 13:23:26 +0900

    パッケージ作成時に作成した仮置きファイルのエラー修正

--- Anken_kanri/jp.co.bsja.anken/action/IndexAction.java
+++ Anken_kanri/jp.co.bsja.anken/action/IndexAction.java
@@ -4,8 +4,8 @@ import org.seasar.struts.annotation.Execute;
 
 public class IndexAction {
 
-	    @Execute(validator = false)
-		public String index() {
-	        return "index.jsp";
+		@Execute(validator = false)
+		public String index(){
+			return"index.jsp";
 		}
 }
