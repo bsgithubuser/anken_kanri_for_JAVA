0fba1ea08344fd96eba24a5a3900914eed0483e9
kensyu_2 <kensyu_2@kensyu-PC>
2016-09-21 14:31:06 +0900

    MenuAction修正

--- Anken_kanri/src/main/java/jp/co/bsja/anken/action/MenuAction.java
+++ Anken_kanri/src/main/java/jp/co/bsja/anken/action/MenuAction.java
@@ -1,13 +1,14 @@
 package jp.co.bsja.anken.action;
 
+import javax.annotation.Resource;
+
 import jp.co.bsja.anken.dto.SessionDto;
 
-import org.seasar.struts.annotation.ActionForm;
 import org.seasar.struts.annotation.Execute;
 
 public class MenuAction {
 
-  @ActionForm
+  @Resource
   public SessionDto sessionDto;
   /**
    * メニュー画面への実行メソッド .
@@ -16,6 +17,16 @@ public class MenuAction {
   //メニュー画面への実行メソッド
   @Execute(validator = false)
   public String index() {
+    //    if (sessionDto.administrativePermission.equals("0")) {
+    //      if (sessionDto.loginData == null) {
+    //        return "/login/login.jsp";
     return "menu.jsp";
+    //      } else {
+    //      // 管理者ではない人が管理者ページに行こうとした場合のエラー
+    //    ActionMessages errors = new ActionMessages();
+    //    errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("MSG_E00003"));
+    //    ActionMessagesUtil.addErrors(RequestUtil.getRequest(), errors);
+    //    return "/error/";
+    //  }
   }
 }
