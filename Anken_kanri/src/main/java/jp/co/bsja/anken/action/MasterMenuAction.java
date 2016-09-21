78db15f8a39eb3b35276ce320018ef84ddd2c36e
kensyu_2 <kensyu_2@BS028>
2016-09-21 14:27:04 +0900

    masterMenuActionにsessionDtoについての処理を追加しました

--- Anken_kanri/src/main/java/jp/co/bsja/anken/action/MasterMenuAction.java
+++ Anken_kanri/src/main/java/jp/co/bsja/anken/action/MasterMenuAction.java
@@ -4,31 +4,32 @@ import javax.annotation.Resource;
 
 import jp.co.bsja.anken.dto.SessionDto;
 
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
 import org.seasar.struts.annotation.Execute;
+import org.seasar.struts.util.ActionMessagesUtil;
+import org.seasar.struts.util.RequestUtil;
 
 public class MasterMenuAction {
 
   @Resource
+  //jspのヘッダーにDtoを使用しているのでここでDtoを宣言する
   public SessionDto sessionDto;
 
   /**
    * マスタ管理画面に遷移するためのメソッドです。
-   * @return 管理者権限を持ち、なおかつログイン情報がある場合はmaster-menu.jspに遷移します。
+   * @return 管理者権限がある場合(adimin = 0)はmaster-menu.jspに遷移します。
    */
   @Execute(validator = false)
   public String index() {
-    //    if (sessionDto.administrativePermission.equals("0")) {
-    //      if (sessionDto.loginData == null) {
-    //        return "/login/login.jsp";
-    //      } else {
-           return "master-menu.jsp";
-    //      }
-    //    } else {
-    //      // 管理者ではない人が管理者ページに行こうとした場合のエラー
-    //    ActionMessages errors = new ActionMessages();
-    //    errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("MSG_E00003"));
-    //    ActionMessagesUtil.addErrors(RequestUtil.getRequest(), errors);
-    //    return "/error/";
-    //  }
+    if (sessionDto.admin.equals("0")) {
+      return "master-menu.jsp";
+    } else {
+      // 管理者ではない人が管理者ページに行こうとした場合のエラー
+      ActionMessages errors = new ActionMessages();
+      errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("MSG_E00003"));
+      ActionMessagesUtil.addErrors(RequestUtil.getRequest(), errors);
+      return "/error/";
+    }
   }
 }
